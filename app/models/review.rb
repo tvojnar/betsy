8b80f41d29508c398027ac6ce30d758393bc89fb
Kimberley Zell <kimberley.zell@gmail.com>
2017-10-18 17:51:59 -0700

    updated review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :product
 
-  validates :rating, presence: {message: "A review must have a rating."}, numericality: { only_integer: true }
+  validates :rating, presence: {message: "A review must have a rating."}, numericality: { only_integer: true, greater_than: 0, less_than: 6 }
 end
