8da80d4bd8024a9c0289b2316fbdf709718cf102
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-18 19:45:47 -0700

    Change .self_from_auth_hash to only set merchant.name to the nickname passed in by github

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,7 +5,8 @@ class Merchant < ApplicationRecord
     merchant = new
     merchant.provider = provider
     merchant.uid = auth_hash['uid']
-    merchant.name = auth_hash['info']['name']
+    # NOTE: we should only set merchant.name to one thing, and since we don't have a username for merchant, we will just set 'name' to the 'nickname' passed in my rails
+    # merchant.name = auth_hash['info']['name']
     merchant.email = auth_hash['info']['email']
     merchant.name = auth_hash['info']['nickname']
 
