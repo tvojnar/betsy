64532af9b94ca2bcbd9f7acf328ed7cfafa20c2f
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-19 22:08:26 -0700

    fixed deltion of session[:order_id] in current_order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020023212) do
+ActiveRecord::Schema.define(version: 20171020045148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20171020023212) do
     t.date "date_submitted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "total_price"
   end
 
   create_table "products", force: :cascade do |t|
