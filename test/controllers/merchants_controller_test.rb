2099f5ba55844d9752d050f35563b0352adc67b1
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-20 11:19:53 -0700

    fixed merge conflict that was left in schema and the extra end in merchants_contreoller_test.rb so that tests will run

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -24,10 +24,8 @@ describe MerchantsController do
       session[:merchant_id].must_equal Merchant.last.id
     end
     #must test that user can log in to be merchant
-<<<<<<< HEAD
-  end # logout
-=======
->>>>>>> 71469abd8a5c1dda6881dce4dd29e5a56ae881db
+
+  end # login
 
   describe "logout" do
     it "can log a user out" do
@@ -47,9 +45,6 @@ describe MerchantsController do
     end # it can log a user out
   end # logout
 
-<<<<<<< HEAD
-end # MerchantsController
-=======
   describe "show" do
       before do
         login(merchants(:diane))
@@ -150,4 +145,3 @@ end # MerchantsController
 end
 
 end
->>>>>>> 71469abd8a5c1dda6881dce4dd29e5a56ae881db
