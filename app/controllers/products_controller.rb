2457ae5cdb53d2c3edec46b1055a37d8609c755e
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-20 11:48:19 -0700

    made changes but now everything is broken

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,11 +8,9 @@ class ProductsController < ApplicationController
     if merchant_id != nil
       merchant = Merchant.find_by(id: merchant_id)
       @products = merchant.products
-      @order_item = current_order.order_items.new
     elsif category_id != nil
       category = Category.find_by(id: category_id)
       @products = category.products
-      @order_item = current_order.order_items.new
     else
       @products = Product.all
     end
@@ -53,6 +51,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find_by(id: params[:id])
+    @order_items = current_order.order_items
     unless @product
       render :root, status: :not_found
     end
