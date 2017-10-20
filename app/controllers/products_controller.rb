a17664a5bb6f35595b71e4745f34e6af257d58f2
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-20 14:12:14 -0700

    prepare to pull master

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,6 @@ class ProductsController < ApplicationController
     elsif category_id != nil
       category = Category.find_by(id: category_id)
       @products = category.products
-
       @order_items = current_order.order_items.new
     elsif params[:merchant_id] && Merchant.find_by(id: params[:merchant_id]) == nil
       flash[:status] = :failure
