05a7c8bbef5369ee9e2bada6d091c93ded44c01a
nkiru onwuneme <generalhrh@gmail.com>
2017-10-21 16:51:13 -0700

    Passing test for valid name required

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,9 +1,15 @@
 require "test_helper"
 
 describe Category do
-  let(:category) { Category.new }
+  let(:category) { Category.new(name: perennials) }
+  let(:c_no_name) {Category.new }
 
-  it "must be valid" do
-    value(category).must_be :valid?
+  describe "validations" do
+    it "requires a name to be valid" do
+      is_valid = c_no_name.valid?
+      is_valid.must_equal false
+      c_no_name.errors.messages.must_include :name
+    end
+
   end
 end
