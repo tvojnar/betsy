578ec0c893525000ca92567777de19b36a61bd8d
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-21 14:35:28 -0700

    Added relationship tests that don't pass because I can't access fixtures in my model tests yet. Added test stubs for validation tests

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -9,16 +9,28 @@ require "test_helper"
 # # end
 
 describe "validations" do
+  it "requires a quantity" do
+    # TODO
+  end # it "requires a quantity" do
+
+  it "requires that quantity is an integer" do
+    # TODO
+  end
+
+  it "requires that quantity is greater than 0" do
+  end # greater than 0 
+
 end # validations
 
 describe "relationships" do
-  let(:pending) {orders :pending}
+  let(:o) {orders :pending}
   let(:prod) {products :spider_plant}
+  # change this to access a fixture once fixtures are working in model tests!
   let(:oi) {OrderItem.new(product_id: prod_id, quantity: 1, order_id: o_id)}
   it "has a product" do
     # QUESTION: why can't I access this fixture? Why does it think order_items is a method?
     # o = orders(:pending)
-    o_id = pending.id
+    # o_id = o.id
     # prod = products(:spider_plant)
     prod_id = prod.id
     # oi = OrderItem.new(product_id: prod_id, quantity: 1, order_id: o_id)
@@ -27,6 +39,14 @@ describe "relationships" do
     oi.must_respond_to :product
     oi.product.must_equal prod
     oi.product_id.must_equal prod.id
-
   end # it "has a product" do
+
+  it "has an order" do
+    # TODO: be able to access fixtures so these tests pass!
+    o_id = o.id
+
+    oi.must_respond_to :order
+    oi.order.must_equal o
+    oi.order_id.must_equal o.id
+  end # it "has an order" do
 end # relationships
