3aec319aa3a879a3621870ed386bdc139065ef74
nkiru onwuneme <generalhrh@gmail.com>
2017-10-21 17:33:27 -0700

    passing test for unique category name

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,8 +1,9 @@
 require "test_helper"
 
 describe Category do
-  let(:category) { Category.new(name: perennials) }
+  let(:category) { Category.new(name: "perennials") }
   let(:c_no_name) {Category.new }
+  let(:c_not_uniq_name) { Category.new(name: "perennials") }
 
   describe "validations" do
     it "creates a new category with a name" do
@@ -15,6 +16,12 @@ describe Category do
       is_valid = c_no_name.valid?
       is_valid.must_equal false
       c_no_name.errors.messages.must_include :name
+    end
+
+    it "requires a unique name to be valid"  do
+      category.save
+      c_not_uniq_name.valid?.must_equal false
+      c_not_uniq_name.errors.messages.must_include :name
     end
 
   end
