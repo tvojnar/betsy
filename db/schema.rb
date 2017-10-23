2c81e0ec7daa18706611140f649a1eb624c0bbf6
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-22 21:50:43 -0700

    changed order_id in order_items table to be an integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171021182317) do
+ActiveRecord::Schema.define(version: 20171023044933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 20171021182317) do
 
   create_table "order_items", force: :cascade do |t|
     t.integer "product_id"
-    t.string "order_id"
+    t.integer "order_id"
     t.integer "quantity"
     t.float "cost"
     t.boolean "shipped_status", default: false
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,8 +2,20 @@ require "test_helper"
 
 describe Product do
   let(:product) { Product.new }
-  #
-  # it "must be valid" do
-  #   value(product).must_be :valid?
-  # end
+
+  describe "relationships" do
+    it "has many orders through order_items" do
+      prod = Product.first
+      prod_id = prod.id
+      o = Order.new
+      o.save!
+
+
+      prod.must_respond_to :orders
+      # prod.orders.must_be :empty?
+      # prod.orders.wont_include o
+      oi = OrderItem.create!(product_id: prod_id, quantity: 1, order: o)
+      prod.orders.must_include o
+    end # has many orders through order_items
+  end # relationships
 end
