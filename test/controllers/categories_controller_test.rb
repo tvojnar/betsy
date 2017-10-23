672c5bc0932190e8c86df9a83679bc15c3ff33e3
nkiru onwuneme <generalhrh@gmail.com>
2017-10-23 02:45:34 -0700

    Test index action

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,9 +1,23 @@
 require "test_helper"
 
 describe CategoriesController do
-  it "should get index" do
-    get categories_index_url
-    value(response).must_be :success?
+  # it "should get index" do
+  #   get categories_index_url
+  #   value(response).must_be :success?
+  # end
+  describe "index" do
+    it "returns a success for all categories" do
+      get categories_path
+      must_respond_with :success
+    end
+  end
+
+  describe "new" do
+
+  end
+
+  describe "create" do
+    
   end
 
 end
