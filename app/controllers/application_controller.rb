091a9f7f2ac41aedd9be29865238860c30c9eaf5
nkiru onwuneme <generalhrh@gmail.com>
2017-10-24 18:25:13 -0700

    fixed typo in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_order
 
-
   # before_action :require_login
-
 
   protected
   def current_order
@@ -22,31 +20,27 @@ class ApplicationController < ActionController::Base
       flash[:message] = "You must be logged in to do that"
       redirect_to root_path
     end
+  end
 
+  # When current_order is called it checks if there is a order_id associated with the session. If there is, it will find that order, if there isn't then it will make a new order
 
-    # When current_order is called it checks if there is a order_id associated with the session. If there is, it will find that order, if there isn't then it will make a new order
-
-
-
-    def save_and_flash(model)
-      result = model.save
-      if result
-        flash[:status] = :success
-        flash[:message] = "Sucessfully saved #{model.class}"
-      else
-        flash.now[:status] = :failure
-        flash.now[:message] = "Failed to save #{model.class}"
-        flash.now[:details] = model.errors.messages
-      end
-      return result
+  def save_and_flash(model)
+    result = model.save
+    if result
+      flash[:status] = :success
+      flash[:message] = "Sucessfully saved #{model.class}"
+    else
+      flash.now[:status] = :failure
+      flash.now[:message] = "Failed to save #{model.class}"
+      flash.now[:details] = model.errors.messages
     end
+    return result
+  end
 
-    def find_merchant
-      if session[:merchant_id] #<<<this will be set in the login method, presumably in the
-        #merchants controller after verifying user is logged in as merchant DL
-        @login_merchant = Merchant.find_by(id: session[:merchant_id])
-      end
-
+  def find_merchant
+    if session[:merchant_id] #<<<this will be set in the login method, presumably in the
+      #merchants controller after verifying user is logged in as merchant DL
+      @login_merchant = Merchant.find_by(id: session[:merchant_id])
     end
   end
 end
