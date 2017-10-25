2d822c7aebf5a16de85a4d2535a8d3fdc89bb9b8
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-25 11:18:13 -0700

    took away unneeded extra named env variables for heroku and removed edites to the omniauth file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.staging? || Rails.env.staging?
-    provider :github, ENV["HEROKU_GITHUB_CLIENT_ID"], ENV["HEROKU_GITHUB_CLIENT_SECRET"], scope: "user:email"
-  else
     provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"], scope: "user:email"
   end
-end
