08f5fb868a2333f2c0108966276c011faf65f9e2
nkiru onwuneme <generalhrh@gmail.com>
2017-10-25 02:00:17 -0700

    validation test for all fields

--- test/models/billing_test.rb
+++ test/models/billing_test.rb
@@ -1,9 +1,24 @@
 require "test_helper"
 
 describe Billing do
-  let(:billing) { Billing.new }
+  let(:product) {Product.first}
+  let(:order) {Order.first}
 
-  it "must be valid" do
-    value(billing).must_be :valid?
+  describe "validations" do
+    it "a new billing is created with all the fields" do
+      b = Billing.new(
+        cc_name: "diane",
+        address: "Fake address st",
+        email: "dl@ada.edu",
+        cc_number:"fakenumber12345",
+        cc_exp: 2017-10-01,
+        cc_cvv: "444",
+        zip: "11111",
+        order: order
+      )
+
+      is_valid = b.valid?
+      is_valid.must_equal true
+    end
   end
 end
