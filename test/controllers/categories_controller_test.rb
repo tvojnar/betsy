283b0f4812bb0d8e692503cbf347d88a9c8f8c50
nkiru onwuneme <generalhrh@gmail.com>
2017-10-25 01:45:23 -0700

    test for invalid data

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -36,49 +36,53 @@ describe CategoriesController do
   end
 
   describe "create" do
-      describe "as a guest" do
-        it "redirects to root" do
-          category_data = {
-            category: {
-              name: "Annual"
-            }
+    describe "as a guest" do
+      it "redirects to root" do
+        category_data = {
+          category: {
+            name: "Annual"
           }
+        }
 
-          start_category_count = Category.count
+        start_category_count = Category.count
 
-          post categories_path, params: category_data
+        post categories_path, params: category_data
 
-          must_respond_with :redirect
-          must_redirect_to root_path
+        must_respond_with :redirect
+        must_redirect_to root_path
 
-          Category.count.must_equal start_category_count
-
-        end
+        Category.count.must_equal start_category_count
       end
-
-    # it "sends bad_request when the category data is bogus" do
-    #   invalid_category_data = {
-    #     category: {
-    #     }
-    #   }
-    #
-    #   Category.new(invalid_category_data[:category]).wont_be :valid?
-    #
-    #   start_category_count = Category.count
-    #
-    #   post categories_path, params: invalid_category_data
-    #
-    #   must_respond_with :bad_request
-    #   Category.count.must_equal start_category_count
-    # end
-
-    # Test is failing
+    end
   end
 
-  # describe "Logged in users" do
-  #   before do
-  #     login(merchants(:nkiru))
-  #   end
-  # end
+    describe "as a logged in merchant" do
+      before do
+        login(merchants(:nkiru))
+      end
 
-end
+      it "adds a category to the DB and redirects when the category data is valid" do
+        category_data = {
+          category: {
+            name: "Annual"
+          }
+        }
+
+        start_category_count = Category.count
+
+        post categories_path, params: category_data
+
+        must_respond_with :redirect
+        must_redirect_to merchant_products_path(merchants(:nkiru))
+
+        Category.count.must_equal start_category_count + 1
+      end
+    end
+
+    # describe "Logged in users" do
+    #   before do
+    #     login(merchants(:nkiru))
+    #   end
+    # end
+
+  end
