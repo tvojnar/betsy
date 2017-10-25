54959f665a41b6407d97c994f6103a4c9fc881fd
nkiru onwuneme <generalhrh@gmail.com>
2017-10-25 02:04:20 -0700

    tested all fields to be validated

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -56,33 +56,41 @@ describe CategoriesController do
     end
   end
 
-    describe "as a logged in merchant" do
-      before do
-        login(merchants(:nkiru))
-      end
+  describe "as a logged in merchant" do
+    before do
+      login(merchants(:nkiru))
+    end
 
-      it "adds a category to the DB and redirects when the category data is valid" do
-        category_data = {
+    it "adds a category to the DB and redirects when the category data is valid" do
+      category_data = {
+        category: {
+          name: "Annual"
+        }
+      }
+
+      start_category_count = Category.count
+
+      post categories_path, params: category_data
+
+      must_respond_with :redirect
+      must_redirect_to merchant_products_path(merchants(:nkiru))
+
+      Category.count.must_equal start_category_count + 1
+    end
+  end
+
+    it "sends bad_request when the category data is invalid" do
+        invalid_category_data = {
           category: {
-            name: "Annual"
+            name: ""
           }
         }
 
         start_category_count = Category.count
 
-        post categories_path, params: category_data
+        post categories_path, params: invalid_category_data
 
-        must_respond_with :redirect
-        must_redirect_to merchant_products_path(merchants(:nkiru))
-
-        Category.count.must_equal start_category_count + 1
+        must_respond_with :bad_request
+        Category.count.must_equal start_category_count
       end
     end
-
-    # describe "Logged in users" do
-    #   before do
-    #     login(merchants(:nkiru))
-    #   end
-    # end
-
-  end
