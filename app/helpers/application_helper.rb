3c0f82d86654cd0f899e1d716b7285e443cc1435
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-25 14:00:03 -0700

    moved logic from view into application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def readable_date(date)
+    ("<span class='date'>" + date.strftime("%b %Y") + "</span>").html_safe
+  end
+
+  def last_4(integer)
+      (integer[-4..-1]).html_safe
+  end # last_4
+
+  def unit_cost(item)
+    item.product.price * item.quantity
+  end
 end
