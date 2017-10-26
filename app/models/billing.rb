0b7591a1b3b4c7642fffab8ca67679e3f145880e
nkiru onwuneme <generalhrh@gmail.com>
2017-10-26 11:46:51 -0700

    Added validation for cc_num

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -2,7 +2,8 @@ class Billing < ApplicationRecord
   belongs_to :order
 
   validates :cc_name, presence: true
-  validates :cc_number, presence: true
+  validates :cc_number, presence: true,
+  length:  {minimum: 4}
   validates :cc_cvv, presence: true
   validates :cc_exp, presence: true
   validates :address, presence: true
